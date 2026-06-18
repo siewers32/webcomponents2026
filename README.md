100920be3313910d50287e01680f094b118dd253
Jan Jaap <janjaap@siewers.org>
2026-06-18 15:51:41 +0200

    fontapp gemaakt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # New Css en Webcomponent tricks (2026)
 
-* Start webserver met: `npx browser-sync start --server --files "index.html, **/*.css, **/*.js`
+* Start webserver met: `npx browser-sync start --server --files "index.html, **/*.css, **/*.js"`
 * Browser-sync wordt dan tijdelijk geinstalleerd (geen node-modules etc.)
--- components/site-nav/site-nav.js
+++ components/site-nav/site-nav.js
@@ -1,11 +1,12 @@
 // site-nav.js
 class SiteNav extends HTMLElement {
   connectedCallback() {
+    const siteName = this.getAttribute('site-name') || 'Sans-serif';
     // Omdat we hier geen Shadow DOM gebruiken, 
     // hoeven we de adoptedStyleSheets niet te zetten.
     // De browser pakt de @scope uit main.css automatisch op.
     this.innerHTML = `
-      <div class="logo">DevStudio</div>
+      <div class="logo">${siteName}</div>
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#projects">Projecten</a></li>
